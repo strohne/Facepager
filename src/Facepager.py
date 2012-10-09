4043892a3150f068f6caefa88007b2acf37f42f8
strohne <strohne@strohne>
2012-10-09 21:09:27 +0200

    batch support, customized columns etc.
    Former-commit-id: 6f904c65f997b2fed5f785484ef9b27c67f9ecc6

--- src/Facepager.py
+++ src/Facepager.py
@@ -1,32 +1,167 @@
+import cProfile
 import sys
 import csv
 from PySide.QtCore import *
 from PySide.QtGui import *
 from toolbar import Toolbar
-from tree import Tree
+from tree import *
+from models import *
+from actions import *
 
 
 class MainWindow(QMainWindow):
     
     def __init__(self,central=None):
         super(MainWindow,self).__init__()
-        self.setWindowTitle("Facepager")
-        self.setWindowIcon(QIcon("./icon_facepager.png"))
-        self.createComponents()
-        self.setMinimumSize(700,400)
-        self.settings=None
-        self.readSettings()
         
-    def createComponents(self):
-        self.Tree=Tree(parent=self)
-        self.Toolbar=Toolbar(parent=self)
-        self.addToolBar(self.Toolbar)    
-        self.setCentralWidget(self.Tree) 
+        self.setWindowTitle("Facepager")                
+        #self.setWindowIcon(QIcon("../icons/icon_facepager.png"))
+        self.setWindowIcon(QIcon("./icon_facepager.png"))        
+        self.setMinimumSize(700,400)
+        
+        self.readSettings() 
+        self.createActions()
+        self.createUI()
+        self.createDB()
+                                        
+        self.updateUI()
+        
+        
+    def createDB(self):
+        self.database = Database()
+        if os.path.isfile(self.settings.value("lastpath")):
+            self.database.connect(self.settings.value("lastpath"))
+        
+        self.treemodel = TreeModel(self,self.database)
+        self.tree.setModel(self.treemodel)        
+        self.actions.actionShowColumns.trigger()
+        
 
-    def createLayouts(self):
-        layoutMain=QHBoxLayout()
-        layoutMain.addWidget(self.Tree)
-        self.setLayout(layoutMain)
+    def createActions(self):
+        self.actions=Actions(self)       
+
+                        
+    def createUI(self):
+        self.toolbar=Toolbar(parent=self,mainWindow=self)
+        self.addToolBar(Qt.LeftToolBarArea,self.toolbar)    
+        
+        self.statusBar().showMessage('No database connection')
+        self.statusBar().setSizeGripEnabled(False)       
+        
+        #dummy widget to contain the layout manager
+        self.mainWidget=QWidget(self) 
+        self.setCentralWidget(self.mainWidget)        
+        mainLayout=QHBoxLayout()
+        self.mainWidget.setLayout(mainLayout)                
+         
+        #tree                                                
+        self.tree=Tree(self.mainWidget,self)
+        mainLayout.addWidget(self.tree,1)
+        
+        #right sidebar
+        detailLayout=QVBoxLayout()
+        mainLayout.addLayout(detailLayout,0)
+        
+        #fetch data
+        detailGroup=QGroupBox("Fetch Data")
+        detailLayout.addWidget(detailGroup)
+        groupLayout=QFormLayout()
+        detailGroup.setLayout(groupLayout)
+                
+        #-Level
+        self.levelEdit=QSpinBox(self.mainWidget)
+        self.levelEdit.setMinimum(1)
+        groupLayout.addRow("Level",self.levelEdit)
+                
+        #-Query Type
+        self.relationEdit=QComboBox(self.mainWidget)
+        self.relationEdit.insertItems(0,['','feed','posts','comments','likes','groups','insights','members','picture','docs','noreply','invited','attending','maybe','declined','videos','accounts','achievements','activities','albums','books','checkins','events','family','friendlists','friends','games','home','interests','links','locations','movies','music','notes','photos','questions','scores','statuses','subscribedto','tagged','television'])        
+        self.relationEdit.setEditable(True)
+        groupLayout.addRow("Query",self.relationEdit)
+
+        #-Since
+        self.sinceEdit=QDateEdit(self.mainWidget)
+        self.sinceEdit.setDate(datetime.datetime.today().replace(month=datetime.datetime.today().month-1))
+        groupLayout.addRow("Since",self.sinceEdit)
+        
+
+        #-Until
+        self.untilEdit=QDateEdit(self.mainWidget)
+        self.untilEdit.setDate(datetime.datetime.today())
+        groupLayout.addRow("Until",self.untilEdit)
+        
+        #-Offset
+        self.offsetEdit=QSpinBox(self.mainWidget)
+        self.offsetEdit.setMaximum(100)
+        self.offsetEdit.setMinimum(0)
+        self.offsetEdit.setValue(0)
+        groupLayout.addRow("Offset",self.offsetEdit)
+
+        #-Limit
+        self.limitEdit=QSpinBox(self.mainWidget)
+        self.limitEdit.setMaximum(500)
+        self.limitEdit.setMinimum(1)
+        self.limitEdit.setValue(100)
+        groupLayout.addRow("Limit",self.limitEdit)
+
+        #-button        
+        button=QPushButton("Fetch Data", self.mainWidget)
+        button.clicked.connect(self.actions.actionQuery.trigger)
+        groupLayout.addRow(button)         
+        
+        #detail data
+        detailGroup=QGroupBox("Raw Data")
+        detailLayout.addWidget(detailGroup)
+        groupLayout=QVBoxLayout()
+        detailGroup.setLayout(groupLayout)
+                        
+        self.detailData=QTextEdit()                        
+        self.detailData.setLineWrapMode(QTextEdit.NoWrap)
+        self.detailData.setWordWrapMode(QTextOption.NoWrap)
+        self.detailData.acceptRichText=False    
+        groupLayout.addWidget(self.detailData)
+        
+        #fields
+        detailGroup=QGroupBox("Custom Table Columns (one key per line)")
+        detailLayout.addWidget(detailGroup)
+        groupLayout=QVBoxLayout()
+        detailGroup.setLayout(groupLayout)
+        
+        self.fieldList=QTextEdit()                        
+        self.fieldList.setLineWrapMode(QTextEdit.NoWrap)
+        self.fieldList.setWordWrapMode(QTextOption.NoWrap)
+        self.fieldList.acceptRichText=False
+        self.fieldList.clear()
+        self.fieldList.append('name')
+        self.fieldList.append('message')
+        self.fieldList.append('type')
+        self.fieldList.append('metadata.type')
+        self.fieldList.append('talking_about_count')
+        self.fieldList.append('likes')                
+        self.fieldList.append('likes.count')        
+        self.fieldList.append('comments.count')
+        self.fieldList.append('created_time')
+        self.fieldList.append('updated_time')
+       
+                    
+        groupLayout.addWidget(self.fieldList)        
+                
+        button=QPushButton("Apply Column Setup")
+        button.clicked.connect(self.actions.actionShowColumns.trigger)
+        groupLayout.addWidget(button)   
+
+        
+      
+
+    def updateUI(self):
+        #disable buttons that do not work without an opened database                   
+        self.actions.databaseActions.setEnabled(self.database.connected)
+        
+        if self.database.connected:
+            self.statusBar().showMessage(self.database.filename)
+        else:
+            self.statusBar().showMessage('No database connection')    
+        
         
     def writeSettings(self):
         self.settings = QSettings("Keyling", "Facepager")
@@ -34,15 +169,14 @@ class MainWindow(QMainWindow):
         self.settings.setValue("size", self.size())
         self.settings.setValue("pos", self.pos())
         self.settings.endGroup()
-        
-     
+             
 
     def readSettings(self):
         self.settings = QSettings("Keyling", "Facepager")
         self.settings.beginGroup("MainWindow")
         self.resize(self.settings.value("size", QSize(400, 400)))
         self.move(self.settings.value("pos", QPoint(200, 200)))
-        self.settings.endGroup()
+        self.settings.endGroup()         
         
 
     def closeEvent(self, event=QCloseEvent()):
@@ -52,10 +186,16 @@ class MainWindow(QMainWindow):
         else:
             event.ignore()
 
-if __name__ == "__main__":
+def startMain():
     app = QApplication(sys.argv)
-    main=MainWindow()
-    
+
+    main=MainWindow()    
     main.show()
     
-    sys.exit(app.exec_())
+    sys.exit(app.exec_())    
+
+  
+if __name__ == "__main__":
+    #cProfile.run('startMain()')
+    startMain()
+
--- src/tree.py
+++ src/tree.py
@@ -2,140 +2,71 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 from models import *
 
-class Tree(QTreeWidget):
+class Tree(QTreeView):
 
-    def __init__(self,parent=None):
+    def __init__(self,parent=None,mainWindow=None):
         super(Tree,self).__init__(parent)
-        self.setColumnCount(15)
-        self.setHeaderLabels(['Site ID','Category', 'Description','Username', 'Website', 'Name','Products','Company_overview', 'Talking_about_count',\
-                                         'Mission', 'Founded', 'Phone', 'Link', 'Likes', 'General_info', 'Checkins'])        
-        self.setSortingEnabled(True)
-        self.itemClicked.connect(self.updateHeader)
-        self.itemActivated.connect(self.updateHeader)
-        self.itemExpanded.connect(self.updateItem)
-
-    
-    @Slot()
-    def updateItem(self,current):
-        if current.type() is 1:
-            self.loadPosts(site_item=current,all=True)
-            
-        elif current.type() is 2:
-            self.loadComments(post_item=current, all=True)
-
-
-    
-    @Slot()
-    def updateHeader(self,item):
-        if item.type() is 1:
-            self.setColumnCount(15)
-
-            self.setHeaderLabels(['Site ID','Category', 'Description','Username', 'Website', 'Name','Products','Company_overview', 'Talking_about_count',\
-                                         'Mission', 'Founded', 'Phone', 'Link', 'Likes', 'General_info', 'Checkins'])
-        elif item.type() is 2:
-            self.setColumnCount(13)
-            self.setHeaderLabels(["Post_ID","Created","Author","Description","Title","Message","Type","Link","Source","Likes","Liker","Shares","Comments Count"])
-        elif item.type() is 3:
-            self.setColumnCount(7)
-            self.setHeaderLabels(["Comment_ID","Created","Author","Message","Likes","Liker"])
-
-       
-    
-    @Slot()
-    def loadSites(self):
-        self.clear()
-        dbsites=Site.query.all()
-        if dbsites:
-            todo=[]
-            for dbsite in dbsites:
-                item_site=QTreeWidgetItem(type=1)
-                item_site.setText(0,dbsite.id)
-                item_site.setText(1,dbsite.category)
-                item_site.setText(2,dbsite.description)
-                item_site.setText(3,dbsite.username)
-                item_site.setText(4,dbsite.website)
-                item_site.setText(5,dbsite.name)
-                item_site.setText(6,dbsite.products)
-                item_site.setText(7,dbsite.company_overview)
-                item_site.setText(8,str(dbsite.talking_about_count))
-                item_site.setText(9,dbsite.mission)
-                item_site.setText(10,dbsite.founded)
-                item_site.setText(11,dbsite.phone)
-                item_site.setText(12,dbsite.link)
-                item_site.setText(13,str(dbsite.likes))
-                item_site.setText(14,dbsite.general_info)
-                item_site.setText(15,str(dbsite.checkins))
+        self.mainWindow=mainWindow
                 
-                for r in range(1,self.columnCount(),1):
-                        item_site.setSizeHint(r,QSize(5,9))
-                        item_site.setBackground(r,QColor(139,136,120))
-                self.loadPosts(item_site,all=False)
-                todo.append(item_site)
-            self.addTopLevelItems(todo)
-        
-    @Slot()    
-    def loadPosts(self,site_item,all=False):
-        if all==True:
-            dbposts=[i for i in Post.query.filter(Post.site_id==site_item.data(0,0)).all()]
-            if dbposts:
-                todo=[]
-                site_item.takeChildren()
-                for  dbpost in dbposts:
-                    item_post=QTreeWidgetItem(type=2)
-                    item_post.setText(0,dbpost.id)
-                    item_post.setText(1,str(dbpost.created_time))
-                    item_post.setText(2,dbpost.author)
-                    item_post.setText(3,dbpost.description)
-                    item_post.setText(4,dbpost.title)
-                    item_post.setText(5,dbpost.message)
-                    item_post.setText(6,dbpost.type)
-                    item_post.setText(7,dbpost.link)
-                    item_post.setText(8,dbpost.source)
-                    item_post.setText(9,str(dbpost.likes))
-                    item_post.setText(10,str(dbpost.liker))
-                    item_post.setText(11,str(dbpost.shares_count))
-                    item_post.setText(12,str(dbpost.comments_count))
-                    for r in range(1,self.columnCount(),1):
-                            item_post.setSizeHint(r,QSize(6,9)) 
-                            item_post.setBackground(r,QColor(205,200,177))
-                    #inaccurate, but way faster!!
-                    if dbpost.comments_count!=0:
-                        item_post.setChildIndicatorPolicy(QTreeWidgetItem.ShowIndicator)
-                    todo.append(item_post)
-            site_item.addChildren(todo)
-        elif all==False:
-            singlepost=Post.query.filter(Post.site_id==site_item.data(0,0)).first()
-            if singlepost is not None:
-                site_item.setChildIndicatorPolicy(QTreeWidgetItem.ShowIndicator)
-        
-                        
-               
+        #self.setSortingEnabled(True)
+        self.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.setSelectionBehavior(QTreeView.SelectRows)
+        self.setUniformRowHeights(True)
+                
 
-
-    
     @Slot()
-    def loadComments(self,post_item,all=False):
-        if all is True:
-            dbcomments=Comment.query.filter(Comment.post_id==post_item.data(0,0)).all()
-            if dbcomments:
-                todo=[]
-                post_item.takeChildren()
-                for dbcom in dbcomments:
-                        item_comment=QTreeWidgetItem(type=3)
-                        item_comment.setText(0,dbcom.id)
-                        item_comment.setText(1,str(dbcom.created_time))
-                        item_comment.setText(2,dbcom.author)
-                        item_comment.setText(3,dbcom.message)
-                        item_comment.setText(4,str(dbcom.likes))
-                        item_comment.setText(5,str(dbcom.liker))
-                        for r in range(1,self.columnCount(),1):
-                             item_comment.setBackground(r,QColor(238,232,205))
-                             item_comment.setSizeHint(r,QSize(6,9))
-                        todo.append(item_comment)
-                post_item.addChildren(todo)
+    def currentChanged(self,current,previous):
+        #show raw data
+        self.mainWindow.detailData.clear()
+        if current.isValid():     
+            item=current.internalPointer()
+            for prop in item.data['response']:
+                self.mainWindow.detailData.append(prop+": "+json.dumps(item.data['response'].get(prop)))
+            
+                
+        #select level
+        level=0
+        c=current
+        while c.isValid():
+            level += 1
+            c=c.parent()
         
-        elif all is False:
-            singlecom=Comment.query.filter(Comment.post_id==post_item.data(0,0)).first()
-            if singlecom is not None:
-                post_item.setChildIndicatorPolicy(QTreeWidgetItem.ShowIndicator)
-    
+        self.mainWindow.levelEdit.setValue(level)
+            
+            
+    def selectedIndexesAndChildren(self,level=None,persistent=False):
+        selected=[x for x in self.selectedIndexes() if x.column()==0]
+        filtered=[]
+
+        def getLevel(index):
+            if not index.isValid():
+                return 0
+            
+            treeitem=index.internalPointer()
+            if (treeitem.data != None) and (treeitem.data['level'] != None):
+                return treeitem.data['level']+1
+            else:
+                return 0
+
+            
+        def addIndex(index):
+            if index not in filtered:
+                if level==None or level==getLevel(index):
+                    if persistent:
+                        filtered.append(QPersistentModelIndex(index))
+                    else:
+                        filtered.append(index)    
+                
+                if level==None or level>getLevel(index) :
+                    self.model().fetchMore(index)
+                    child=index.child(0,0)
+                    while child.isValid():
+                        addIndex(child)
+                        child=index.child(child.row()+1,0)
+                            
+        
+        for index in selected:
+            addIndex(index)
+            
+        return filtered     
+  
