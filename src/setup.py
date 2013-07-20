ff8682f0695d580dd55dae89d16b07d7e0f75ea2
strohne <strohne@strohne>
2013-07-20 09:37:18 +0200

    new build

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from cx_Freeze import setup, Executable
 
 exe = Executable(
         script="Facepager.py",
-        #base="Win32GUI",
+        base="Win32GUI",
         icon="../icons/icon_facepager.ico",
         copyDependentFiles = True,
         targetDir="build"
