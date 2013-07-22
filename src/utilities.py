2cfe960599103f1022ea58d4023d11f3342e2beb
Till Keyling <till.keyling@googlemail.com>
2013-07-22 23:24:44 +0200

    edit dictgetter method

--- src/utilities.py
+++ src/utilities.py
@@ -1,3 +1,4 @@
+import json
 
 def getDictValue(data,multikey):
     keys=multikey.split('.')                
@@ -6,10 +7,13 @@ def getDictValue(data,multikey):
         if type(value) is dict:
             value=value.get(key,"")
         elif type(value) is list:
-            try:
-                value=value[int(key)]
-            except:
-                return ""        
+            valuelist=[]
+            for elem in value:
+                if key in elem:
+                    valuelist.append(elem[key])
+                else:
+                    valuelist.append(elem)
+            return ";".join(str(vlelem) for vlelem in valuelist)
         else:
             return ""
     if type(value) is dict:
