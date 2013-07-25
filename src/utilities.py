4bcc812f259a7f4c7e6fbcd45290f8dacf0c61b3
strohne <strohne@strohne>
2013-07-25 11:52:07 +0200

    Version control for presets, bug fixes

--- src/utilities.py
+++ src/utilities.py
@@ -1,37 +1,40 @@
 import json
 
 def getDictValue(data,multikey,dump=True):
-    keys=multikey.split('.',1)
-                
-    if type(data) is dict and keys[0] != '':
-        value=data.get(keys[0],"")
-        if len(keys) > 1:            
-            value = getDictValue(value,keys[1])  
-            
-    elif type(data) is list and keys[0] != '':
-        try:
-            value=data[int(keys[0])]
+    try:
+        keys=multikey.split('.',1)
+                    
+        if type(data) is dict and keys[0] != '':
+            value=data.get(keys[0],"")
             if len(keys) > 1:            
-                value = getDictValue(value,keys[1]) 
-        except:                                
-            if keys[0] == '*' and len(keys) > 1:
-                listkey = keys[1]
-            elif keys[0] == '*':
-                listkey = ''
-            else:
-                listkey = keys[0]        
-                        
-            valuelist=[]
-            for elem in data:
-                valuelist.append(getDictValue(elem,listkey))                                     
-            value = ";".join(valuelist)
-
-    else:
-        value = data
-        
-    if dump and (type(value) is dict or type(value) is list):
-        return json.dumps(value) 
-    else:        
-        return value   
+                value = getDictValue(value,keys[1],dump)  
+                
+        elif type(data) is list and keys[0] != '':
+            try:
+                value=data[int(keys[0])]
+                if len(keys) > 1:            
+                    value = getDictValue(value,keys[1],dump) 
+            except:                                
+                if keys[0] == '*' and len(keys) > 1:
+                    listkey = keys[1]
+                elif keys[0] == '*':
+                    listkey = ''
+                else:
+                    listkey = keys[0]        
+                            
+                valuelist=[]
+                for elem in data:
+                    valuelist.append(getDictValue(elem,listkey,dump))                                     
+                value = ";".join(valuelist)
+    
+        else:
+            value = data
+            
+        if dump and (type(value) is dict or type(value) is list):
+            return json.dumps(value) 
+        else:        
+            return value
+    except Exception as e:
+        return ""
 
 
