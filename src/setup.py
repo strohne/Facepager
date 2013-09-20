73ae5dd7e7a428b423de213d37e0de978dce21d6
strohne <strohne@strohne>
2013-09-20 11:13:28 +0200

    new build (version 3.3)

--- src/setup.py
+++ src/setup.py
@@ -23,7 +23,7 @@ buildoptions = {
 
 setup(
         name = "Facepager 3",
-        version = "3.1",
+        version = "3.3",
         description = "The Facebook Page Crawler",
         options = {'build_exe': buildoptions},
         executables = [exe]
