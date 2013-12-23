44991a323f4bf67556938ebab578e70655092ce2
strohne <strohne@strohne>
2013-12-23 23:01:23 +0100

    prepare select nodes window, fix pagination issue
    Former-commit-id: 9bccb3374c0f8b033fa707336708b0d57316133c

---
+++ src/selectnodes.py
@@ -0,0 +1,70 @@
+from PySide.QtCore import *
+from PySide.QtWebKit import *
+from PySide.QtGui import *
+from paramedit import *
+
+class SelectNodesWindow(QDialog):
+    
+    
+    def __init__(self, parent=None):
+        super(SelectNodesWindow,self).__init__(parent)
+        
+        self.mainWindow = parent
+        self.setWindowTitle("Select Nodes")
+        
+        
+        #layout
+        layout = QVBoxLayout(self)        
+        central = QHBoxLayout()
+        layout.addLayout(central,1)
+        self.setLayout(layout)
+        
+        settingsLayout=QFormLayout()
+        central.addLayout(settingsLayout,1)
+        
+        #Params
+        self.paramEdit = QParamEdit(self)
+        self.paramEdit.setNameOptions(['<None>','since','until','offset','limit','type'])
+        self.paramEdit.setValueOptions(['<None>','2013-07-17','page'])       
+        settingsLayout.addRow("Parameters",self.paramEdit)
+        
+        #Checkboxes
+        self.childrenCheckbox = QCheckBox('Apply to current selection and child nodes')
+        self.childrenCheckbox.setChecked(True)
+        settingsLayout.addRow(self.childrenCheckbox)
+        
+        #Level
+        self.levelEdit=QSpinBox(self)
+        self.levelEdit.setMinimum(1)        
+        self.levelEdit.setMaximum(500)
+        settingsLayout.addRow("Level",self.levelEdit)        
+        
+        #buttons
+        buttons=QDialogButtonBox()
+        self.selectButton = QPushButton('Apply Selection')        
+        self.selectButton.clicked.connect(self.selectNodes)
+        buttons.addButton(self.selectButton,QDialogButtonBox.ActionRole)
+        
+       
+        buttons.addButton(QDialogButtonBox.Cancel)
+        buttons.rejected.connect(self.close)                        
+        layout.addWidget(buttons,0)
+
+    def show(self):
+        self.exec_()
+        
+    def selectNodes(self):  
+        level = self.levelEdit.value()-1      
+        filter = {'level':level,'objecttype':['seed','data','unpacked']}
+        indexes = self.mainWindow.tree.selectedIndexesAndChildren(False,filter)
+        
+        selmod = self.mainWindow.tree.selectionModel()
+        selmod.clear()
+        for index in indexes:        
+            selmod.select(index, QItemSelectionModel.Select|QItemSelectionModel.Rows)
+                
+        self.close()
+                        
+        
+        
+            
