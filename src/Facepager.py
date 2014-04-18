60ad8f89eb7930d8924fe0c38308e97d828e1f15
strohne <krimskram@freenet.de>
2014-04-18 14:54:50 +0200

    bug fix: create log file dir at first run

--- src/Facepager.py
+++ src/Facepager.py
@@ -8,7 +8,7 @@ from datatree import *
 from dictionarytree import *
 from database import *
 from actions import *
-from apimodules import * 
+from apimodules import *
 from help import *
 from presets import *
 from timer import *
@@ -17,39 +17,39 @@ import logging
 import threading
 
 class MainWindow(QMainWindow):
-    
+
     def __init__(self,central=None):
         super(MainWindow,self).__init__()
-        
-        self.setWindowTitle("Facepager 3.5")                
-        self.setWindowIcon(QIcon(":/icons/icon_facepager.png"))        
+
+        self.setWindowTitle("Facepager 3.5")
+        self.setWindowIcon(QIcon(":/icons/icon_facepager.png"))
         self.setMinimumSize(900,700)
         self.move(QDesktopWidget().availableGeometry().center() - self.frameGeometry().center()-QPoint(0,100))
 #        self.setStyleSheet("* {font-size:27px;}")
         #self.deleteSettings()
         self.lock_logging = threading.Lock()
-        self.readSettings() 
+        self.readSettings()
         self.createActions()
         self.createUI()
-        self.createDB()                                        
+        self.createDB()
         self.updateUI()
-        
-    def createDB(self):        
+
+    def createDB(self):
         self.database = Database(self)
         lastpath = self.settings.value("lastpath")
         if lastpath and os.path.isfile(lastpath):
-            self.database.connect(self.settings.value("lastpath"))        
-        
+            self.database.connect(self.settings.value("lastpath"))
+
         self.tree.loadData(self.database)
         self.actions.actionShowColumns.trigger()
-        
+
 
     def createActions(self):
-        self.actions=Actions(self)       
+        self.actions=Actions(self)
 
-                        
+
     def createUI(self):
-        
+
         self.helpwindow=HelpWindow(self)
         self.presetWindow=PresetWindow(self)
         self.timerWindow=TimerWindow(self)
@@ -60,21 +60,21 @@ class MainWindow(QMainWindow):
         self.timerWindow.timercountdown.connect(self.actions.timerCountdown)
         self.timerWindow.timerfired.connect(self.actions.timerFired)
         self.timerStatus = QLabel("Timer stopped ")
-        self.statusBar().addPermanentWidget(self.timerStatus)        
+        self.statusBar().addPermanentWidget(self.timerStatus)
         self.toolbar=Toolbar(parent=self,mainWindow=self)
-        self.addToolBar(Qt.TopToolBarArea,self.toolbar)    
+        self.addToolBar(Qt.TopToolBarArea,self.toolbar)
 
         self.selectionStatus = QLabel("0 node(s) selected ")
         self.statusBar().addPermanentWidget(self.selectionStatus)
         self.statusBar().showMessage('No database connection')
         self.statusBar().setSizeGripEnabled(False)
-        
+
         #dummy widget to contain the layout manager
-        #self.mainWidget=QWidget(self) 
+        #self.mainWidget=QWidget(self)
         self.mainWidget=QSplitter(self)
-        self.mainWidget.setOrientation(Qt.Vertical)        
-        self.setCentralWidget(self.mainWidget)        
-        
+        self.mainWidget.setOrientation(Qt.Vertical)
+        self.setCentralWidget(self.mainWidget)
+
         #top widget
         topWidget=QWidget(self)
         self.mainWidget.addWidget(topWidget)
@@ -82,27 +82,27 @@ class MainWindow(QMainWindow):
         bottomWidget=QWidget(self)
         self.mainWidget.addWidget(bottomWidget)
         self.mainWidget.setStretchFactor(0, 1);
-        
+
         #mainLayout=QVBoxLayout()
-        #self.mainWidget.setLayout(mainLayout)                
-         
-         
+        #self.mainWidget.setLayout(mainLayout)
+
+
         dataLayout=QHBoxLayout()
         topWidget.setLayout(dataLayout)
         dataSplitter = QSplitter(self)
         dataLayout.addWidget(dataSplitter)
-        
+
         requestLayout=QHBoxLayout()
         bottomWidget.setLayout(requestLayout)
 
-         
+
         #tree
         dataWidget=QWidget()
         dataLayout=QVBoxLayout()
         dataLayout.setContentsMargins(0,0,0,0)
         dataWidget.setLayout(dataLayout)
         dataSplitter.addWidget(dataWidget)
-        dataSplitter.setStretchFactor(0, 1);                                                              
+        dataSplitter.setStretchFactor(0, 1);
 
         treetoolbar = QToolBar(self)
         treetoolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon);
@@ -113,67 +113,67 @@ class MainWindow(QMainWindow):
 
         self.tree=DataTree(self.mainWidget)
         self.tree.nodeSelected.connect(self.actions.treeNodeSelected)
-        dataLayout.addWidget(self.tree)  
-           
-        
-        #button=QToolButton(self.mainWidget)
-        #button.setIcon(QIcon(":/icons/timer.png"))        
+        dataLayout.addWidget(self.tree)
 
-        
+
+        #button=QToolButton(self.mainWidget)
+        #button.setIcon(QIcon(":/icons/timer.png"))
+
+
         #right sidebar
         detailWidget=QWidget()
         detailLayout=QVBoxLayout()
         detailLayout.setContentsMargins(11,0,0,0)
         detailWidget.setLayout(detailLayout)
-        dataSplitter.addWidget(detailWidget)        
+        dataSplitter.addWidget(detailWidget)
         dataSplitter.setStretchFactor(1, 0);
-     
+
         detailtoolbar = QToolBar(self)
         detailtoolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon);
         detailtoolbar.setIconSize(QSize(16,16))
 
         detailtoolbar.addActions(self.actions.detailActions.actions())
         detailLayout.addWidget (detailtoolbar)
-        
+
         self.detailTree=DictionaryTree(self.mainWidget)
         detailLayout.addWidget(self.detailTree)
 
-        
+
         #requests
         actionlayout=QVBoxLayout()
         requestLayout.addLayout(actionlayout,1)
-        
+
         self.RequestTabs=QTabWidget()
-        actionlayout.addWidget(self.RequestTabs)        
+        actionlayout.addWidget(self.RequestTabs)
         self.RequestTabs.addTab(FacebookTab(self),"Facebook")
         self.RequestTabs.addTab(TwitterTab(self),"Twitter")
-        self.RequestTabs.addTab(GenericTab(self),"Generic")    
+        self.RequestTabs.addTab(GenericTab(self),"Generic")
         self.RequestTabs.addTab(FilesTab(self),"Files")
-        self.RequestTabs.addTab(TwitterStreamingTab(self),"Twitter Streaming")    
-    
-        #fetch data      
+        self.RequestTabs.addTab(TwitterStreamingTab(self),"Twitter Streaming")
+
+        #fetch data
         f=QFont()
         f.setPointSize(11)
 
-                                     
+
         fetchdata=QHBoxLayout()
         fetchdata.setContentsMargins(10,0,10,0)
-        actionlayout.addLayout(fetchdata)        
-        #fetchdata.addStretch(1)                         
-                                    
+        actionlayout.addLayout(fetchdata)
+        #fetchdata.addStretch(1)
+
         #-Level
         self.levelEdit=QSpinBox(self.mainWidget)
         self.levelEdit.setMinimum(1)
         self.levelEdit.setFont(f)
         self.levelEdit.setMinimumHeight(35)
         label=QLabel("For all selected nodes and \ntheir children of level")
-        #label.setFont(f)    
- 
+        #label.setFont(f)
+
         #label.setWordWrap(True)
         #label.setMaximumWidth(100)
         fetchdata.addWidget(label,0)
         fetchdata.addWidget(self.levelEdit,0)
-        
+
         #-button
         button=QPushButton(QIcon(":/icons/fetch.png"),"Fetch Data", self.mainWidget)
         button.setToolTip("Fetch data from the API with the current settings")
@@ -182,7 +182,7 @@ class MainWindow(QMainWindow):
         button.clicked.connect(self.actions.actionQuery.trigger)
         button.setFont(f)
         fetchdata.addWidget(button,1)
-        
+
         #-timer button
         button=QToolButton(self.mainWidget)
         button.setIcon(QIcon(":/icons/timer.png"))
@@ -190,16 +190,16 @@ class MainWindow(QMainWindow):
         button.setIconSize(QSize(25,25))
         button.clicked.connect(self.actions.actionTimer.trigger)
         fetchdata.addWidget(button,1)
-        
+
         #Status
-        statusLayout=QVBoxLayout()        
+        statusLayout=QVBoxLayout()
         requestLayout.addLayout(statusLayout,2)
 
         detailGroup=QGroupBox("Status Log")
         groupLayout=QVBoxLayout()
         detailGroup.setLayout(groupLayout)
         statusLayout.addWidget(detailGroup,1)
-                
+
         self.loglist=QTextEdit()
         self.loglist.setLineWrapMode(QTextEdit.NoWrap)
         self.loglist.setWordWrapMode(QTextOption.NoWrap)
@@ -212,8 +212,8 @@ class MainWindow(QMainWindow):
         requestLayout.addWidget(detailGroup)
         groupLayout=QVBoxLayout()
         detailGroup.setLayout(groupLayout)
-        
-        self.fieldList=QTextEdit()                        
+
+        self.fieldList=QTextEdit()
         self.fieldList.setLineWrapMode(QTextEdit.NoWrap)
         self.fieldList.setWordWrapMode(QTextOption.NoWrap)
         self.fieldList.acceptRichText=False
@@ -223,65 +223,75 @@ class MainWindow(QMainWindow):
         self.fieldList.append('type')
         self.fieldList.append('metadata.type')
         self.fieldList.append('talking_about_count')
-        self.fieldList.append('likes')                
-        self.fieldList.append('likes.count')        
+        self.fieldList.append('likes')
+        self.fieldList.append('likes.count')
         self.fieldList.append('shares.count')
         self.fieldList.append('comments.count')
         self.fieldList.append('created_time')
         self.fieldList.append('updated_time')
-       
-        self.fieldList.setPlainText(self.settings.value('columns',self.fieldList.toPlainText()))       
-                    
-        groupLayout.addWidget(self.fieldList)        
-                
+
+        self.fieldList.setPlainText(self.settings.value('columns',self.fieldList.toPlainText()))
+
+        groupLayout.addWidget(self.fieldList)
+
         button=QPushButton("Apply Column Setup")
         button.setToolTip("Apply the columns to the cental data view. New columns may be hidden and are appended on the right side")
         button.clicked.connect(self.actions.actionShowColumns.trigger)
-        groupLayout.addWidget(button)           
-                
+        groupLayout.addWidget(button)
+
     def updateUI(self):
-        #disable buttons that do not work without an opened database                   
+        #disable buttons that do not work without an opened database
         self.actions.databaseActions.setEnabled(self.database.connected)
-        
+
         if self.database.connected:
             self.statusBar().showMessage(self.database.filename)
         else:
-            self.statusBar().showMessage('No database connection')    
-   
-        
+            self.statusBar().showMessage('No database connection')
+
+
     def writeSettings(self):
         QCoreApplication.setOrganizationName("Keyling")
         QCoreApplication.setApplicationName("Facepager")
 
-        self.settings = QSettings()        
+        self.settings = QSettings()
         self.settings.beginGroup("MainWindow")
         self.settings.setValue("size", self.size())
-        self.settings.setValue("pos", self.pos())        
+        self.settings.setValue("pos", self.pos())
         self.settings.setValue("version","3.0")
         self.settings.endGroup()
-        
+
         self.settings.setValue('columns',self.fieldList.toPlainText())
-        
+
         for i in range(self.RequestTabs.count()):
             self.RequestTabs.widget(i).saveSettings()
-        
+
     def readSettings(self):
         QSettings.setDefaultFormat(QSettings.IniFormat)
         QCoreApplication.setOrganizationName("Keyling")
         QCoreApplication.setApplicationName("Facepager")
-        self.settings = QSettings()        
+        self.settings = QSettings()
         self.settings.beginGroup("MainWindow")
-        
+
         #self.resize(self.settings.value("size", QSize(800, 800)))
         #self.move(self.settings.value("pos", QPoint(200, 10)))
-        self.settings.endGroup()         
-      
-    def deleteSettings(self):    
-        self.settings = QSettings("Keyling", "Facepager")
-        self.settings.beginGroup("MainWindow")
-        self.settings.remove("")
         self.settings.endGroup()
-        self.settings.remove("lastpath")
+
+    def deleteSettings(self):
+        QSettings.setDefaultFormat(QSettings.IniFormat)
+        QCoreApplication.setOrganizationName("Keyling")
+        QCoreApplication.setApplicationName("Facepager")
+        self.settings = QSettings()
+
+        self.settings.clear()
+        self.settings.sync()
+
+        #self.settings.beginGroup("ApiModule_Facebook")
+        #print self.settings.allKeys()
+
+#         self.settings.beginGroup("MainWindow")
+#         self.settings.remove("")
+#         self.settings.endGroup()
+#         self.settings.remove("lastpath")
 
     def closeEvent(self, event=QCloseEvent()):
         if self.close():
@@ -289,18 +299,18 @@ class MainWindow(QMainWindow):
             event.accept()
         else:
             event.ignore()
-            
+
     @Slot(str)
     def logmessage(self,message):
         with self.lock_logging:
-            if isinstance(message,Exception):          
-                self.loglist.append(str(datetime.now())+" Exception: "+str(message))          
+            if isinstance(message,Exception):
+                self.loglist.append(str(datetime.now())+" Exception: "+str(message))
                 logging.exception(message)
-                                        
+
             else:
                 self.loglist.append(str(datetime.now())+" "+message)
-        
-                 
+
+
 class Toolbar(QToolBar):
     """
     Initialize the main toolbar for the facepager - that provides the central interface and functions.
@@ -310,36 +320,37 @@ class Toolbar(QToolBar):
         self.mainWindow=mainWindow
         self.setToolButtonStyle(Qt.ToolButtonTextBesideIcon);
         self.setIconSize(QSize(24,24))
-        
-        self.addActions(self.mainWindow.actions.basicActions.actions())        
+
+        self.addActions(self.mainWindow.actions.basicActions.actions())
         self.addSeparator()
         self.addActions(self.mainWindow.actions.databaseActions.actions())
-        
+
         self.addSeparator()
-        #self.addAction(self.mainWindow.actions.actionExpandAll)        
+        #self.addAction(self.mainWindow.actions.actionExpandAll)
         #self.addAction(self.mainWindow.actions.actionCollapseAll)
         #self.addAction(self.mainWindow.actions.actionSelectNodes)
         self.addAction(self.mainWindow.actions.actionLoadPreset)
         self.addAction(self.mainWindow.actions.actionHelp)
-    
 
-            
+
+
 def startMain():
     app = QApplication(sys.argv)
 
-    main=MainWindow()    
+    main=MainWindow()
     main.show()
-    
-    sys.exit(app.exec_())    
 
-  
-if __name__ == "__main__":    
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
     try:
         logfolder = os.path.join(os.path.expanduser("~"),'Facepager','Logs')
-        if not os.path.isdir(logfolder): os.mkdir(logfolder)    
+        if not os.path.isdir(logfolder):
+            os.makedirs(logfolder)
         logging.basicConfig(filename=os.path.join(logfolder,'facepager.log'),level=logging.ERROR,format='%(asctime)s %(levelname)s:%(message)s')
-    except:
-        print "Error intitializing log file"
+    except Exception as e:
+        print u"Error intitializing log file: {}".format(e.message)
     finally:
         #cProfile.run('startMain()')
         #yappi.start()
