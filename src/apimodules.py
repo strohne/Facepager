9485874ae13910889e0095d22134b114482f0c08
strohne <krimskram@freenet.de>
2015-03-21 11:33:54 +0100

    removed page limit
    Former-commit-id: d41835da248c112154ec7985ddf1b60e8d4cd286

--- src/apimodules.py
+++ src/apimodules.py
@@ -339,7 +339,7 @@ class FacebookTab(ApiTab):
         # Pages Box
         self.pagesEdit = QSpinBox(self)
         self.pagesEdit.setMinimum(1)
-        self.pagesEdit.setMaximum(500)
+        #self.pagesEdit.setMaximum(500)
 
         # Login-Boxes
         self.tokenEdit = QLineEdit()
@@ -509,7 +509,7 @@ class TwitterTab(ApiTab):
         # Pages-Box
         self.pagesEdit = QSpinBox(self)
         self.pagesEdit.setMinimum(1)
-        self.pagesEdit.setMaximum(500)
+        #self.pagesEdit.setMaximum(500)
 
         # LogIn Box
         self.tokenEdit = QLineEdit()
