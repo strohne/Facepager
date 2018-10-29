6bed58731298e9ed27de80a9ce66ddf5bb22cd21
strohne <krimskram@freenet.de>
2018-10-29 12:26:24 +0100

    update build
    Former-commit-id: 2a542ce76b4d96a42f74a0c014da04a517f96871

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -8,6 +8,7 @@ python setup_windows.py build
 cd build
 copy Facepager.exe exe.win32-2.7
 copy python27.dll exe.win32-2.7
+copy ..\..\build\windows\dlls\*.* exe.win32-2.7
 
 copy ..\..\build\windows\Facepager_Setupscript.nsi exe.win32-2.7\Facepager_Setupscript.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" exe.win32-2.7\Facepager_Setupscript.nsi
