61ba8dc290bc8243a1adb8dc3c72b0aa0efe7239
Jakob Jünger <krimskram@freenet.de>
2022-03-28 16:25:00 +0200

    Build without upx

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -5,8 +5,8 @@ rmdir /s / q build
 rmdir /s / q dist
 
 copy ..\build\windows\Facepager.spec Facepager.spec
-pyinstaller --noconfirm --debug all --upx-exclude vcruntime140.dll --upx-dir ..\build\windows\upx-3.96-win64\ Facepager.spec
 
+pyinstaller --noconfirm --debug all Facepager.spec
 @rem pyinstaller --debug all --noconfirm --upx-exclude vcruntime140.dll Facepager.spec --upx-dir ..\build\windows\upx-3.96-win64\
 
 cd dist
